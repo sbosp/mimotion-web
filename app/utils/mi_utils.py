4467d06b4f5afee4ed4ab5f54624f11a3fde53ed
asherqu <asherqu@tencent.com>
2026-06-17 11:51:08 +0800

    catch 防护

--- app/utils/mi_utils.py
+++ app/utils/mi_utils.py
@@ -3,15 +3,15 @@ import app.utils.mi_motion2 as mitask2
 
 
 def run(task_value: dict):
-    message, login_token, userid, app_token, status, cache, step_count = mitask.MiMotionRunner(
-        task_value).login_and_post_step()
+    message, login_token, userid, app_token, status, cache, step_count = mitask2.MiMotion(
+        check_item=task_value).main()
     print(message, status, step_count, cache, userid, app_token, login_token, )
     task_value['login_token'] = login_token or ''
     task_value['userid'] = userid or ''
     task_value['app_token'] = app_token or ''
     if not status:
-        message, login_token, userid, app_token, status, cache, step_count = mitask2.MiMotion(
-            check_item=task_value).main()
+        message, login_token, userid, app_token, status, cache, step_count = mitask.MiMotionRunner(
+            task_value).login_and_post_step()
         print(message, status, step_count, cache, userid, app_token, login_token, )
         task_value['login_token'] = login_token or ''
         task_value['userid'] = userid or ''
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     print(run({
         'mi_user': '3379174616@qq.com',
         'mi_password': 'qq123456',
-        'min_step': 6666,
-        'max_step': 7777,
+        'min_step': 16666,
+        'max_step': 17777,
     }))
